62e9e45ab4315a148bdd01782a7993f58e935959
yashaswipratick <yashaswi.pratick@gmail.com>
2025-11-28 02:20:31 +0530

    Added code

---
+++ coding-revision/src/com/revision/topics/heap/Pair.java
@@ -0,0 +1,12 @@
+package com.revision.topics.heap;
+
+public class Pair {
+
+    int left;
+    int right;
+
+    public Pair(int left, int right) {
+        this.left = left;
+        this.right = right;
+    }
+}
