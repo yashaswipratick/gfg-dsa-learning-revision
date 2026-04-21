fbeacdaa6a9739a1a6b4430f4153a9048b3387bb
yashaswipratick <yashaswi.pratick@gmail.com>
2026-04-22 01:37:14 +0530

    Added new code

--- coding-revision/src/com/revision/topics/array/revision_april_2026/MaxAndSecondMax_22.java
+++ coding-revision/src/com/revision/topics/array/revision_april_2026/MaxAndSecondMax_22.java
@@ -5,10 +5,26 @@ import java.util.ArrayList;
 public class MaxAndSecondMax_22 {
 
     private static ArrayList<Integer> largestAndSecondLargest(int arr[]) {
+        int max = -1;
+        int secondMax = -1;
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                secondMax = max;
+                max = arr[i];
+            } else if (arr[i] > secondMax && arr[i] < max) {
+                secondMax = arr[i];
+            }
+        }
+        list.add(max);
+        list.add(secondMax);
 
+        return list;
     }
 
     public static void main(String[] args) {
-
+        largestAndSecondLargest(new int[]{2, 1, 2});
+        largestAndSecondLargest(new int[]{1, 2, 3, 4, 5});
+        largestAndSecondLargest(new int[]{3, 3, 3});
     }
 }
---
+++ coding-revision/src/com/revision/topics/array/revision_april_2026/MeanAndMedian_23.java
@@ -0,0 +1,40 @@
+package com.revision.topics.array.revision_april_2026;
+
+import java.util.Arrays;
+
+// PL - https://www.geeksforgeeks.org/batch/dsa-4/track/DSASP-Arrays/problem/mean-and-median-1587115620
+public class MeanAndMedian_23 {
+
+    public static int mean(int[] arr) {
+        // code here
+        int sum = 0;
+
+        for(int i : arr) {
+            sum += i;
+        }
+        return sum/arr.length;
+    }
+
+    public static int median(int[] arr) {
+        Arrays.sort(arr);
+        if(arr.length % 2 == 0) {
+            return (arr[(arr.length / 2)] + arr[(arr.length / 2) - 1]) / 2;
+        } else {
+            return arr[arr.length / 2];
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(mean(new int[]{2}));
+        System.out.println(median(new int[]{2}));
+
+        System.out.println(mean(new int[]{2,3}));
+        System.out.println(median(new int[]{2,3}));
+
+        System.out.println(mean(new int[]{1, 2, 19, 28, 5}));
+        System.out.println(median(new int[]{1, 2, 19, 28, 5}));
+
+        System.out.println(mean(new int[]{2, 8, 3, 4}));
+        System.out.println(median(new int[]{2, 8, 3, 4}));
+    }
+}
